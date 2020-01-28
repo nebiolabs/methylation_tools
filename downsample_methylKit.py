9549138d8b785b65eaeb01923158ba639d721fcf
Matt Campbell <mcampbell@neb.com>
2020-01-28 09:42:52 -0500

    Removing old comments

--- downsample_methylKit.py
+++ downsample_methylKit.py
@@ -9,18 +9,16 @@ if len(sys.argv) != 2:
 	print("Usage: cat <methylkit file> | grep -v chrBase | downsample_methylKit.py <fraction of reads to keep> > <output file>")
 	sys.exit()
 
-downsample_len = len(sys.argv[1].split(".")[1])
+downsample_len = len(sys.argv[1].split(".")[1]) # Counts number of decimal places, for downsampling precision
 downsample_number = int(sys.argv[1].split(".")[1])
-downsample_cap = int("".join(["1" ,"".join(["0" for x in range(downsample_len)])]))
+downsample_cap = int("".join(["1" ,"".join(["0" for x in range(downsample_len)])])) # An order of magnitude larger than downsample_len
 
 
 counter = 0
-#print(next(sys.stdin).strip())
 for line in sys.stdin:
 	counter += 1
 	if counter % 1000000 == 0:
 		print("\rProcessed {} lines".format(counter), end = "", file = sys.stderr)
-# 	print(line.strip())
 	meth = []
 	unmeth = []
 
@@ -29,34 +27,24 @@ for line in sys.stdin:
 	covg = int(line[4])
 	meth_fraction = float(line[5]) / 100
 	unmeth_fraction = float(line[6]) / 100
-# 	print(covg, meth_fraction, unmeth_fraction)
 	
 	meth_covg = int(round(covg * meth_fraction))
 	unmeth_covg = int(round(covg * unmeth_fraction))
-# 	print(meth_covg, unmeth_covg)
 	
 	meth = [1] * meth_covg
 	unmeth = [1] * unmeth_covg
-# 	print(meth, unmeth)
 	
 	for x in range(len(meth)):
 		tmp = random.randint(0, downsample_cap)
-# 		print(tmp)
 		if tmp > downsample_number:
-# 			print("Removing one methylated coverage")
 			meth[x] = 0
 	
 	for x in range(len(unmeth)):
 		tmp = random.randint(0, downsample_cap)
-# 		print(tmp)
 		if tmp > downsample_number:
-# 			print("Removing one unmethylated coverage")
 			unmeth[x] = 0
 
-# 	print(meth, unmeth)
-	
 	new_covg = sum(meth) + sum(unmeth)
-# 	print(new_covg)
 	
 	new_meth_fraction = line[5]
 	new_unmeth_fraction = line[6]
@@ -64,7 +52,6 @@ for line in sys.stdin:
 	if new_covg > 0:
 		new_meth_fraction = round(float(sum(meth) / new_covg) * 100, 2)
 		new_unmeth_fraction = round(float(sum(unmeth) / new_covg) * 100, 2)
-# 		print(new_meth_fraction, new_unmeth_fraction)
 	
 		line[4] = str(new_covg)
 		line[5] = str(new_meth_fraction)
